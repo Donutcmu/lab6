e200897d208e214a042f54edecb86b1d3fbb16e2
Donut <punnatat_ng@cmu.ac.th>
2023-12-01 15:19:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int even=0;
+    int odd=0;
+    int number;
+    while(number != 0){
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number!=0){
+            if(number%2==0){
+                even+=1;
+            }
+            else{
+                odd+=1;
+            }
+        }
+    }
+    cout << "#Even numbers = "<<even;
+    cout<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
